6c7e80751e73d7f8684e5c8742adaa3c6a362e7a
Paweł Budzianowski <pfb30@cam.ac.uk>
2018-11-01 16:11:08 +0100

    Update train.py

--- train.py
+++ train.py
@@ -23,9 +23,9 @@ parser.add_argument('--attention_type', type=str, default='bahdanau')
 parser.add_argument('--use_emb',  type=util.str2bool, nargs='?', const=True, default=False)
 
 parser.add_argument('--emb_size', type=int, default=50)
-parser.add_argument('--hid_size_enc', type=int, default=50)
-parser.add_argument('--hid_size_dec', type=int, default=50)
-parser.add_argument('--hid_size_pol', type=int, default=50)
+parser.add_argument('--hid_size_enc', type=int, default=150)
+parser.add_argument('--hid_size_dec', type=int, default=150)
+parser.add_argument('--hid_size_pol', type=int, default=150)
 parser.add_argument('--db_size', type=int, default=30)
 parser.add_argument('--bs_size', type=int, default=94)
 
